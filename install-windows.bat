ff3055e78fcc21f602f08283972d24e86e988d33
Himashana <himashanasuraweera5@gmail.com>
2026-02-19 09:32:11 +0530

    feat: updated the installation script to setup in AppData

--- install-windows.bat
+++ install-windows.bat
@@ -2,8 +2,11 @@
 setlocal
 
 set "PYTHON_URL=https://www.python.org/ftp/python/3.14.3/python-3.14.3-amd64.exe"
-set "PYTHON_INSTALLER=python-installer.exe"
+set "PYTHON_INSTALLER=%TEMP%\python-3.14.3-amd64.exe"
 set "PYTHON_SHA256=b68ad91421afbbd1a628105199c8c5f6179b21ba799067a8d8c0bbac3b7defb0"
+
+@REM Installation path
+set "INSTALL_DIR=%LOCALAPPDATA%\CHN Software Developers\AETManager"
 
 @REM Check for existing Python installation
 python --version >nul 2>&1
@@ -37,6 +40,41 @@ if errorlevel 1 (
 
 @REM Install the python dependencies for the project
 echo Installing python dependencies...
-pip install -r requirements.txt
+
+@REM Check is pip is in the PATH
+pip --version >nul 2>&1
+
+@REM If pip is not found, use python -m pip to install dependencies
+if errorlevel 1 (
+    echo pip is not found in PATH. Using python -m pip instead.
+    python -m pip install -r requirements.txt
+) else (
+    pip install -r requirements.txt
+)
+
 echo Python dependencies installed successfully.
 
+@REM Copy the software files to the installation directory without installation and requirements files
+echo Copying files to installation directory...
+if not exist "%INSTALL_DIR%" (
+    mkdir "%INSTALL_DIR%"
+)
+robocopy . "%INSTALL_DIR%" /E /XF install-windows.bat requirements.txt >nul
+if errorlevel 8 (
+    echo Failed to copy files to installation directory.
+    exit /b 1
+)
+
+@REM Add the installation directory to the PATH environment variable
+echo Adding installation directory to PATH environment variable...
+powershell -NoProfile -ExecutionPolicy Bypass -Command "$installDir='%INSTALL_DIR%'; $userPath=[Environment]::GetEnvironmentVariable('Path','User'); if ([string]::IsNullOrWhiteSpace($userPath)) { $userPath='' }; if (($userPath -split ';') -notcontains $installDir) { $newPath = if ([string]::IsNullOrEmpty($userPath)) { $installDir } else { $userPath.TrimEnd(';') + ';' + $installDir }; [Environment]::SetEnvironmentVariable('Path', $newPath, 'User') }"
+if errorlevel 1 (
+    echo Failed to update PATH environment variable.
+    exit /b 1
+)
+set "PATH=%PATH%;%INSTALL_DIR%"
+
+echo Installation complete.
+
+echo Press any key to exit...
+pause >nul
